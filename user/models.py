572258c71c9416a4bfd8a31729f618d6c5f0b609
BoringMind <super.d3280@ya.ru>
2020-07-20 12:25:43 +0300

    feet: added user model

--- user/models.py
+++ user/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class ProfileManager(models.Manager):
+    def create(self, username, email, photo=None):
+        """Always create User and Profile instances in tandem."""
+        user = User(username=username, email=email)
+        user.save()
+        profile = UserProfile(user=user, photo=photo)
+        profile.save()
+        return profile
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    photo = models.ImageField(upload_to='uploads/', blank=True)
+    objects = ProfileManager()
